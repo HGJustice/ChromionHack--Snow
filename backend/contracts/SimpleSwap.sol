eb9c74ff9f9cb982f95ee6b9dc8f5efcbeffff3b
HGJustice <125309498+HGJustice@users.noreply.github.com>
2025-06-02 22:35:03 +0100

    got aave lping to work through smart contract

--- backend/contracts/SimpleSwap.sol
+++ backend/contracts/SimpleSwap.sol
@@ -24,19 +24,6 @@ interface ILBRouter {
         address to,
         uint256 deadline
     ) external returns (uint256 amountOut);
-
-    function getSwapOut(
-        address pair,
-        uint128 amountIn,
-        bool swapForY
-    )
-        external
-        view
-        returns (
-            uint128 amountInLeft,
-            uint128 amountOut,
-            uint128 amountOutLeft
-        );
 }
 
 contract SimpleSwap {
@@ -44,16 +31,12 @@ contract SimpleSwap {
         ILBRouter(0x18556DA13313f3532c54711497A8FedAC273220E);
     IERC20 public USDC = IERC20(0xB6076C93701D6a07266c31066B298AeC6dd65c2d);
     IERC20 public WAVAX = IERC20(0xd00ae08403B9bbb9124bB305C09058E32C39A48c);
-    address public pair = 0x88F36a6B0e37E78d0Fb1d41B07A47BAD3D995453; // AVAX-USDC 20bps pool
 
     function swapUSDCForAVAX(uint128 amountIn) external {
-        // Transfer USDC from user
         USDC.transferFrom(msg.sender, address(this), amountIn);
 
-        // Approve router
         USDC.approve(address(router), amountIn);
 
-        // Setup path exactly like docs
         IERC20[] memory tokenPath = new IERC20[](2);
         tokenPath[0] = USDC;
         tokenPath[1] = WAVAX;
@@ -64,13 +47,11 @@ contract SimpleSwap {
         ILBRouter.Version[] memory versions = new ILBRouter.Version[](1);
         versions[0] = ILBRouter.Version.V2_1;
 
-        ILBRouter.Path memory path; // instanciate and populate the path to perform the swap.
+        ILBRouter.Path memory path;
         path.pairBinSteps = pairBinSteps;
         path.versions = versions;
         path.tokenPath = tokenPath;
 
-        // (, uint256 amountOut, ) = router.getSwapOut(pair, amountIn, true);
-        // uint256 amountOutWithSlippage = amountOut * 99 / 100; // We allow for 1% slippage
         router.swapExactTokensForNATIVE(
             amountIn,
             0,
